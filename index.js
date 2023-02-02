cdf8332e4a7d46ded55e181de8d5878d0d6965ca
Draian123 <beiteldennis3@yahoo.de>
2023-02-02 14:06:48 +0100

    finished

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Make sure to require your model so you can connect to the DB
 const { Model } = require("mongoose");
+const Viking = require("./models/User");
 
 //This line of code connects to the Database
 require("./db");
@@ -7,3 +8,41 @@ require("./db");
 //Iteration 2:
 
 //Iteration 3:
+const data = { firstName: 'Ragnar', familyName: 'Josh', strength: 20, weapon: 'Jaw' };
+const data2 = [{firstName: 'Odin', familyName: 'Joshua', strength: 10}, {firstName:'Istvan', familyName: 'Hun', strength:200000},
+{firstName: 'Dennis', familyName: 'Merkel'} ] 
+// Mongoose allows us to use a callback pattern
+// to handle the completion of the asynchronous operation
+// Viking.create(data, (error, viking) => {
+//   if (error) {
+//     console.log('An error happened:', error);
+//     return;
+//   }
+//   console.log('The viking is saved and its value is: ', viking);
+// //   Viking.findByIdAndRemove('63db9dad558257ae74dece82')
+// //     .then(viking=> console.log('The viking is deleted: ', viking))
+// //     .catch(error => console.log('An error happened:', error));
+
+//     Viking.deleteOne({ firstName: viking.firstName})
+//     .then(viking=> console.log('The viking is deleted: ', viking))
+//     .catch(error => console.log('An error happened:', error));  
+// });
+Viking.insertMany(data2, (error, viking) => {
+    if (error) {
+      console.log('An error happened:', error);
+      return;
+    }
+    console.log('The viking is saved and its value is: ', viking);
+    Viking.updateOne({ firstName:'Istvan' }, { firstName:'Odin', strength: 10 })
+    .then(viking=> console.log('The viking is updated: ', viking))
+    .catch(error => console.log('An error happened:', error));
+
+    Viking.find({ name: 'Odin' })
+  .then(user => {
+    // handle users
+    console.log(user)
+  })
+  .catch(error => {
+    // handle error
+  });
+  });
--- models/User.js
+++ models/User.js
@@ -1,5 +1,27 @@
 // require mongoose and destructor Schema and model from it
 const { Schema, model } = require("mongoose");
+var mongoose = require('mongoose');
+
 //Create a viking schema here
+const vikingSchema = new Schema({
+    firstName: {
+      type: String,
+      required: true
+    },
+    familyName: {
+      type: String,
+      required: true
+    },
+    strength: {
+      type: Number,
+    },
+    weapon:{
+        type: String
+    }
+  });
+
 //<======Don't forget Module.export ============>
+const Viking = mongoose.model('Viking', vikingSchema);
+ 
+module.exports = Viking;
 //<================Schema=====================>
